fdf9281f4587bb5ecfc583386d6048c7dea090ec
code-bhuvan <bhuvan.karthik.20102012@gmail.com>
2025-09-24 13:44:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeColor() {
+  // Generate random values for R, G, B (0–255)
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  // Create the RGB color string
+  const randomColor = `rgb(${r}, ${g}, ${b})`;
+    const fontrandomColor = (`rgb(${b},${r}, ${g})`);
+
+  // Apply it to the body background
+  document.body.style.backgroundColor = randomColor;
+ // apply it to the text color
+  document.body.style.color = fontrandomColor;
+}
